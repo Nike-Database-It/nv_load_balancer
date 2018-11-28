0fe517f87f75afdfb4767b30cd47778d169574a7
Neil Vodoor <nvodoor@gmail.com>
2018-11-28 14:19:20 -0800

    Updated Server

--- servers/server.js
+++ servers/server.js
@@ -9,6 +9,8 @@ const app = express();
 
 let server = 1;
 
+let secReq = 1;
+
 app.get('/:productSku/colors', (req, res) => {
   if (server === 1) {
     axios.get(`http://ec2-52-91-19-251.compute-1.amazonaws.com:3006/${req.params.productSku }/colors`)
@@ -24,6 +26,26 @@ app.get('/:productSku/colors', (req, res) => {
       .then((resp) => {
         res.status(200).send(resp.data);
         server = 1;
+      })
+      .catch(err => res.status(500).end(err.message))
+  }
+})
+
+app.get('/:productSku/colors/:style', (req, res) => {
+  if (secReq === 1) {
+    axios.get(`http://ec2-52-91-19-251.compute-1.amazonaws.com:3006/${req.params.productSku}/colors/${req.params.style}`)
+      .then((resp) => {
+        res.status(200).send(resp.data);
+        secReq = 1;
+      })
+      .catch(err => res.status(500).end(err.message))
+  }
+
+  if (secReq === 2) {
+    axios.get(`http://ec2-18-208-180-115.compute-1.amazonaws.com:3006/${req.params.productSku}/colors/${req.params.style}`)
+      .then((resp) => {
+        res.status(200).send(resp.data);
+        secReq = 1;
       })
       .catch(err => res.status(500).end(err.message))
   }
